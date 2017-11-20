e0c60fd6bcfc7629399c851793aad0b007e5ccc6
Juancho Baz <juancho1494@gmail.com>
2017-11-20 13:35:55 -0300

    Pa subir

--- workstation/src/servidor/Publicador.java
+++ workstation/src/servidor/Publicador.java
@@ -60,7 +60,7 @@ public class Publicador {
 			e.printStackTrace();
 		}
     	
-    	endpoint = Endpoint.publish("http://localhost:11115/publicador", this);
+    	endpoint = Endpoint.publish("http://"+p.getProperty("ip")+":"+p.getProperty("port")+"/publicador", this);
     }
 
     @WebMethod(exclude = true)
